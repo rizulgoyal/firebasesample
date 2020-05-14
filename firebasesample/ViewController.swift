5924ba0c28d8517b4af6d8aab89cd747b7ca0435
rizul <rizulgoyal23@gmail.com>
2020-05-14 10:39:16 -0400

    Initial Commit

---
+++ firebasesample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebasesample
+//
+//  Created by Rizul goyal on 2020-05-14.
+//  Copyright © 2020 Rizul goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
